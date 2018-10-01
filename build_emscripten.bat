5926e6f6f7c1ac1e970f04c5ec5c1b45b5517bd9
Diharaw <shredhead94@gmail.com>
2018-10-02 00:39:40 +0530

    Updated Emscripten build script.

--- build_emscripten.bat
+++ build_emscripten.bat
@@ -1,3 +1,5 @@
 cd build_em
-cmake .. -DBUILD_SAMPLES=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_NO_EXPORT=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\emscripten\1.38.12\cmake\Modules\Platform\Emscripten.cmake -G "NMake Makefiles"
+call emsdk activate latest
+call vcvarsamd64_x86
+cmake .. -DBUILD_SAMPLES=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_NO_EXPORT=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_TOOLCHAIN_FILE=C:\emsdk\emscripten\1.37.28\cmake\Modules\Platform\Emscripten.cmake -G "NMake Makefiles"
 timeout /t -1
